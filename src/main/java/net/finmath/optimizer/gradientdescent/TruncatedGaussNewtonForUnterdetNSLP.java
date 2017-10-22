885a396caf8f47cefeaf40e7d8150fe76135fb2a
Stefan Sedlmair <stefan.sedlmair@campus.lmu.de>
2017-10-22 21:37:37 +0200

    rename and now extends the AbstraceGradientDescentScalarOptimization class

---
+++ src/main/java/net/finmath/optimizer/gradientdescent/TruncatedGaussNewtonForUnterdetNSLP.java
@@ -0,0 +1,45 @@
+package net.finmath.optimizer.gradientdescent;
+
+import java.util.concurrent.ExecutorService;
+
+import net.finmath.functions.VectorAlgbra;
+import net.finmath.optimizer.SolverException;
+
+/**
+ * Implements the Truncated Gauß-Newton Algorithm
+ * stated in the article "<a href="http://www.sciencedirect.com/science/article/pii/S016892741630157X">Approximate Gauss-Newton methods for 
+ * solving underdetermined non-linear least squares problems </a>"
+ * from J.-F. Bao et al.
+ * 
+ * It is claimed that this algorithm performs well for a highly underdetermined NLSP.
+ * 
+ * @author Stefan Sedlmair
+ * @version 0.1
+ * */
+public abstract class TruncatedGaussNewtonForUnterdetNSLP extends  AbstractGradientDescentScalarOptimization{
+	
+	private static final long serialVersionUID = 7043579549858250182L;
+
+	public TruncatedGaussNewtonForUnterdetNSLP(double[] initialParameter, double targetValue, double errorTolerance,
+			int maxNumberOfIterations, 
+			double[] finiteDifferenceStepSizes, 
+			ExecutorService executor) {
+		super(initialParameter, targetValue, errorTolerance, maxNumberOfIterations,	finiteDifferenceStepSizes, 
+				executor, false);
+	}
+
+	public TruncatedGaussNewtonForUnterdetNSLP(double[] initialParameters, double targetValue, double errorTolerance,
+			int maxIterations) {
+		this(initialParameters, targetValue, errorTolerance, maxIterations, null, null);
+	}
+
+	@Override
+	protected double getStepSize(double[] parameter) throws SolverException {
+		
+		double value = getValue(parameter);
+		double[] derivative = getDerivative(parameter);
+		
+		return value / VectorAlgbra.innerProduct(derivative, derivative);
+	}
+}
+
