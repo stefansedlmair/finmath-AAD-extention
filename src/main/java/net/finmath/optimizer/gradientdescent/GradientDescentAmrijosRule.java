4e2bcf88da068741d786f34901ff928f4203cfc3
Stefan Sedlmair <stefan.sedlmair@campus.lmu.de>
2017-10-22 21:39:01 +0200

    now extends the new rename and now extends the AbstraceGradientDescentScalarOptimization class

--- src/main/java/net/finmath/optimizer/gradientdescent/GradientDescentAmrijosRule.java
+++ src/main/java/net/finmath/optimizer/gradientdescent/GradientDescentAmrijosRule.java
@@ -1,220 +1,57 @@
 package net.finmath.optimizer.gradientdescent;
 
-import java.io.Serializable;
-import java.util.Arrays;
-import java.util.Vector;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
 
-import org.apache.commons.math3.linear.Array2DRowRealMatrix;
-import org.apache.commons.math3.linear.ArrayRealVector;
-import org.apache.commons.math3.linear.RealMatrix;
-import org.apache.commons.math3.linear.RealVector;
-
-import net.finmath.montecarlo.RandomVariable;
-import net.finmath.optimizer.OptimizerInterface;
-import net.finmath.optimizer.OptimizerInterfaceAAD;
+import net.finmath.functions.VectorAlgbra;
 import net.finmath.optimizer.SolverException;
-import net.finmath.optimizer.OptimizerInterfaceAAD.DerivativeFunction;
 
-public abstract class GradientDescentAmrijosRule implements Serializable, Cloneable, OptimizerInterface, DerivativeFunction {
+public abstract class GradientDescentAmrijosRule extends AbstractGradientDescentScalarOptimization {
 
-	private static final long serialVersionUID = -84822697392025037L;
+	private static final long serialVersionUID = -8770177009110584745L;
 
-	private double targetAccuracy;
-	private double minAdvancePerStep;
+	public GradientDescentAmrijosRule(
+			double[] initialParameter, double targetValue, double errorTolerance,
+			int maxNumberOfIterations, 
+			double[] finiteDifferenceStepSizes, 
+			ExecutorService executor, 
+			boolean allowWorsening) {
+		super(initialParameter, targetValue, errorTolerance, maxNumberOfIterations, finiteDifferenceStepSizes, executor, allowWorsening);
+	}
 	
-	private double currentAccuracy		= Double.POSITIVE_INFINITY;
-	private double bestAccuracy 		= Double.POSITIVE_INFINITY;
-	private int numberOfIterations		= 0;
-	private int maxNumberOfIterations;
+	public GradientDescentAmrijosRule(double[] initialParameters, double targetValue, double errorTolerance, int maxIterations) {
+		super(initialParameters, targetValue, errorTolerance, maxIterations, null, null, false);
+	}
 
-
-	private RealVector bestParameters;
-	private RealVector currentParameters;
-
-	private double finiteDifferenceStepSize;
-
-	private double   currentValue;
-	private double   targetValue;
-
-	private double	beta				= 1E-2;
-	private double  sigma				= 1E-2;
+	private final double alpha				= 1E-1;	/* no good value suggested */
+	private final double c1					= 1E-4; /* for linear problems (see p39 in Numerical Optimization) */
 	
-	private int maxL 					= 100;
-
-	RealVector derivativeStorageVector;
-
-	private ExecutorService executor;
-
-	public GradientDescentAmrijosRule(double[] initialParameters, double targetValue, double finiteDifferenceStepSize, double targetAccuracy, double minAdvancePerStep ,int maxNumberOfIterations) {
-		this.bestParameters = new ArrayRealVector(initialParameters);
-		this.currentParameters = new ArrayRealVector(initialParameters);
-
-		this.targetValue = targetValue;
-		this.minAdvancePerStep = minAdvancePerStep;
-		
-		this.finiteDifferenceStepSize = finiteDifferenceStepSize;
-
-		this.targetAccuracy = targetAccuracy;
-		this.maxNumberOfIterations = maxNumberOfIterations;
-	}
-
-	public GradientDescentAmrijosRule(double[] initialParameters, double targetValue, double minAdvancePerStep, double targetAccuracy, int maxNumberOfIterations) {
-		this(initialParameters, targetValue, Double.NaN, targetAccuracy,minAdvancePerStep ,maxNumberOfIterations);
-	}
+	private final int maxL 					= 10;
 
 	@Override
-	public double[] getBestFitParameters() {
-		return bestParameters.toArray();
-	}
+	protected double getStepSize(double[] parameter) throws SolverException {
+		double leftSide, rightSide ;
+		double l = 3; // start with l=4 to jump not too fast
 
-	@Override
-	public double getRootMeanSquaredError() {
-		return bestAccuracy;
-	}
-
-	@Override
-	public int getIterations() {
-		return numberOfIterations;
-	}
-
-	@Override
-	public void run() throws SolverException {
+		double[] derivative = getDerivative(parameter);
+		double 	 value = getValue(parameter);
 		
-		currentValue = getValue(currentParameters);
-		currentAccuracy = Math.abs(currentValue - targetValue);
-		bestAccuracy = currentAccuracy;
+		double[] newPossibleParameter;
+		double betaL = Double.NaN;
 		
-		System.out.println("Step#: " +numberOfIterations + "\t currentAccuracy: " + currentAccuracy);
-		
-		while(!isDone()){
-			numberOfIterations++;
-
-			RealMatrix derivativeMatrix = getDerivative(currentParameters);
-			derivativeStorageVector = derivativeMatrix.getColumnVector(0);
-
-			double leftSide, rightSide ;
-			double l = 1;
-
-			RealVector newPossibleParameters;
+		do{
+			betaL = Math.pow(alpha, l++);
 			
-			do{
-				double betaL = Math.pow(beta, l++);
-				newPossibleParameters = currentParameters.subtract(derivativeStorageVector.mapMultiplyToSelf(betaL));
-				leftSide = getValue(newPossibleParameters);
-				rightSide = currentValue - sigma * betaL * derivativeStorageVector.dotProduct(derivativeStorageVector);
-//				System.out.println(betaL + "\t" + leftSide + "\t" + rightSide);
-			} while((leftSide > rightSide || Double.isNaN(leftSide)) && l < maxL);
-
-			currentParameters = newPossibleParameters.copy();
-			currentValue = leftSide;
-			currentAccuracy = Math.abs(currentValue - targetValue);
-					
-			System.out.println("Step#: " +numberOfIterations + "\t currentAccuracy: " + currentAccuracy + "\t l=" + l);
-	
+			newPossibleParameter = VectorAlgbra.subtract(parameter, VectorAlgbra.scalarProduct(betaL, derivative));
 			
-			if(currentAccuracy < bestAccuracy){
-				double bestAccuracy = this.bestAccuracy;
-				this.bestAccuracy = currentAccuracy;
-				bestParameters = currentParameters.copy();
-				if(Math.abs(bestAccuracy - currentAccuracy) < minAdvancePerStep) break;
-			}
-			else break;
-		}
-
-	}
-	
-	private double getValue(RealVector parameter) throws SolverException{
-		double[] values = new double[1];
-		setValues(parameter.toArray(), values);
-		return values[0];
-	}
-	
-	private RealMatrix getDerivative(RealVector parameter) throws SolverException{
-		double[][] derivative = new double[currentParameters.getDimension()][1 /*only one dimension*/];
-		setDerivatives(parameter.toArray(), derivative);
-		return new Array2DRowRealMatrix(derivative, false /*copy array*/);
-	}
+			leftSide = getValue(newPossibleParameter);
+						
+			rightSide = value - c1 * betaL * VectorAlgbra.innerProduct(derivative, derivative);
+			
+		} while((leftSide > rightSide || Double.isNaN(leftSide)) && l <= maxL);
 		
-	@Override
-	public abstract void setValues(double[] parameters, double[] values) throws SolverException;
-
-	@Override
-	public void setDerivatives(double[] parameters, double[][] derivatives) throws SolverException {
-		// Calculate new derivatives. Note that this method is called only with
-		// parameters = parameterCurrent, so we may use valueCurrent.
-
-		Vector<Future<double[]>> valueFutures = new Vector<Future<double[]>>(currentParameters.getDimension());
-		for (int parameterIndex = 0; parameterIndex < currentParameters.getDimension(); parameterIndex++) {
-			final double[] parametersNew	= parameters.clone();
-			final double[] derivative		= derivatives[parameterIndex];
-
-			final int workerParameterIndex = parameterIndex;
-			Callable<double[]> worker = new  Callable<double[]>() {
-				public double[] call() throws SolverException {
-					double parameterFiniteDifference;
-					if(!Double.isNaN(finiteDifferenceStepSize)) {
-						parameterFiniteDifference = finiteDifferenceStepSize;
-					}
-					else {
-						/*
-						 * Try to adaptively set a parameter shift. Note that in some
-						 * applications it may be important to set parameterSteps.
-						 * appropriately.
-						 */
-						parameterFiniteDifference = (Math.abs(parametersNew[workerParameterIndex]) + 1) * 1E-8;
-					}
-
-					// Shift parameter value
-					parametersNew[workerParameterIndex] += parameterFiniteDifference;
-
-					// Calculate derivative as (valueUpShift - valueCurrent) / parameterFiniteDifference
-					try {
-						setValues(parametersNew, derivative);
-					} catch (Exception e) {
-						// We signal an exception to calculate the derivative as NaN
-						Arrays.fill(derivative, Double.NaN);
-					}
-
-					derivative[0] -= currentValue;
-					derivative[0] /= parameterFiniteDifference;
-					if(Double.isNaN(derivative[0])) derivative[0] = 0.0;
-
-					return derivative;
-				}
-			};
-			if(executor != null) {
-				Future<double[]> valueFuture = executor.submit(worker);
-				valueFutures.add(parameterIndex, valueFuture);
-			}
-			else {
-				FutureTask<double[]> valueFutureTask = new FutureTask<double[]>(worker);
-				valueFutureTask.run();
-				valueFutures.add(parameterIndex, valueFutureTask);
-			}
-		}
-
-		for (int parameterIndex = 0; parameterIndex < currentParameters.getDimension(); parameterIndex++) {
-			try {
-				derivatives[parameterIndex] = valueFutures.get(parameterIndex).get();
-			}
-			catch (InterruptedException e) {
-				throw new SolverException(e);
-			} catch (ExecutionException e) {
-				throw new SolverException(e);
-			}
-		}
-	}		
-
-	private boolean isDone(){
-		return currentAccuracy < targetAccuracy 
-				||
-				maxNumberOfIterations < numberOfIterations
-				;
+		System.out.println("l = " + l);
+		
+		return betaL;
 	}
 }
 
